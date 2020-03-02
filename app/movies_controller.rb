062cd62b985646f2ef0b13a70b737f7f56b12e1c
James Benjamin <james.abenj@gmail.com>
2020-03-02 18:55:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.find_by_id(1)
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
@@ -49,7 +49,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by_id(1)
 end
 
 def can_find_by_multiple_attributes
